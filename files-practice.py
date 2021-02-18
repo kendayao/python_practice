40ce616bbaf3b02ea222bc72e5b552808b851aa3
Kaeneth Dayao <kendayao@gmail.com>
2021-02-18 11:51:51 -0800

    read/write files python practice

---
+++ files-practice.py
@@ -0,0 +1,28 @@
+# open file
+
+# myfile=open('myfile.txt')
+# contents=myfile.read()
+
+# myfile.readlines() puts contents in a list
+
+# print(contents)
+
+# myfile.close()
+
+# with open ('myfile.txt') as my_new_file:
+#     contents=my_new_file.readlines()
+#     print(contents)
+
+
+# READ
+with open('myfile.txt', mode='r') as my_file:
+    contents=my_file.read()
+    print(contents)
+
+# APPEND
+# with open('myfile.txt', mode='a') as my_file:
+#     my_file.write('\nThis is line 4')
+
+# WRITE overwrites if existing or creates new file
+# with open('myfile.txt', mode='w') as my_file:
+#     my_file.write('\nThis is line 4')
